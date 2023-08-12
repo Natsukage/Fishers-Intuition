5eaccc612e3d6b1946dd855c9a6f22937b391e49
Nukoooo <nukoooo@nukooo.cc>
2023-08-12 19:50:41 +0800

    sigs

--- 渔人的直感/Models/Common.cs
+++ 渔人的直感/Models/Common.cs
@@ -8,7 +8,7 @@ namespace 渔人的直感.Models
     /// </summary>
     public static class Data
     {
-        
+
         public const int FishEyesBuffId = 762; // 50; //      鱼眼的Buff ID
 
         public static List<SpecialWeather> SpecialWeathers = new List<SpecialWeather>();
@@ -19,11 +19,18 @@ namespace 渔人的直感.Models
         //public static IntPtr BuffTablePtr;
         private static IntPtr weatherPtr;
 
+        private static IntPtr eventFrameworkPtr;
+        private static short contentDirectorOffset;
+        private static short oceanFishingTimeOffsetOffset;
+        private static short contentTimeLeftOffset;
+        private static short contentDirectorTypeOffset;
+
         public static IntPtr WeatherPtr
         {
             get => _scanner.ReadIntPtr(weatherPtr) + 0x20;
         }
 
+        public static IntPtr OceanFishingContentDirector => GetInstanceContentDirector();
 
         public const int UiStatusEffects = 6488; //UIStatusEffects相对于ActorTable的偏移。此值随着版本更新随时可能发生改变。
 
@@ -42,8 +49,18 @@ namespace 渔人的直感.Models
             //Weather用于获取当前天气，判断幻海流、空岛特殊天气触发等。
             weatherPtr = scanner.GetStaticAddressFromSig("48 8D 0D ? ? ? ? 0F 28 DE") + 8;
 
+            //获取EventFrameworkPtr
+            eventFrameworkPtr = scanner.GetStaticAddressFromSig("48 8B 35 ?? ?? ?? ?? 0F B6 EA 4C 8B F1");
 
-            SpecialWeathers.Add(new SpecialWeather {Id = 145, Name = "幻海流", Duration = 120f});
+            //获取Offset相关
+            contentDirectorOffset = scanner.ReadInt16(scanner.ScanText("48 83 B9 ?? ?? ?? ?? ?? 74 ?? B0 ?? C3 48 8B 81"), 3);
+            oceanFishingTimeOffsetOffset = scanner.ReadInt16(scanner.ScanText("89 83 ? ? ? ? 48 89 83 ? ? ? ? 48 89 83 ? ? ? ? 88 83 ? ? ? ? 89 83 ? ? ? ? 88 83"), 2);
+            contentTimeLeftOffset = scanner.ReadInt16(scanner.ScanText("F3 0F 10 81 ?? ?? ?? ?? 0F 2F C4"), 4);
+            contentDirectorTypeOffset = scanner.ReadInt16(scanner.ScanText("80 B9 ?? ?? ?? ?? ?? 48 8B D9 75 ?? 48 8B 49 ?? BA"), 2);
+
+            Console.WriteLine($@"contentDirectorTypeOffset: 0x{contentDirectorTypeOffset:X}, contentTimeLeftOffset: 0x{contentTimeLeftOffset:X}, oceanFishingTimeOffsetOffset: 0x{oceanFishingTimeOffsetOffset:X}, contentDirectorOffset: 0x{contentDirectorOffset:X}");
+
+            SpecialWeathers.Add(new SpecialWeather { Id = 145, Name = "幻海流", Duration = 120f });
             if (Properties.Settings.Default.CheckDiademWeather)
             {
                 SpecialWeathers.Add(new SpecialWeather { Id = 133, Name = "灵烈火", Duration = 600f });
@@ -54,6 +71,34 @@ namespace 渔人的直感.Models
             //SpecialWeathers.Add(new SpecialWeather { Id = 1, Name = "碧空（测试）", Duration = 60f });
             //SpecialWeathers.Add(new SpecialWeather { Id = 2, Name = "晴朗（测试）", Duration = 60f });
         }
+
+        //基本上是rebuild了这个函数 E8 ?? ?? ?? ?? 0F B6 98
+        private static IntPtr GetInstanceContentDirector()
+        {
+            //找不到EventFrameworkPtr
+            if (eventFrameworkPtr == IntPtr.Zero)
+                return IntPtr.Zero;
+
+            var directorPtr = _scanner.ReadIntPtr(eventFrameworkPtr, contentDirectorOffset);
+            //找不到ContentDirector
+            if (directorPtr == IntPtr.Zero)
+                return IntPtr.Zero;
+
+            //检查Director类型是否为InstanceContent
+            var type = BitConverter.ToUInt16(_scanner.ReadBytes(_scanner.ReadIntPtr(directorPtr, 8) + 2, 2), 0);
+            if (type != 0x8003)
+                return IntPtr.Zero;
+
+            //检查InstanceContent的类型是否为OceanFishing
+            if (_scanner.ReadByte(directorPtr, contentDirectorTypeOffset) != 16)
+            {
+
+                return IntPtr.Zero;
+            }
+
+            return directorPtr;
+        }
+
     }
 
     public struct SpecialWeather
